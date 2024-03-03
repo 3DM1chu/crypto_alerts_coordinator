1e5e0960478ad7b398ba8843702d30402a0d06d6
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-03-03 13:16:19 +0100

    Reorganization

--- redis_test_1.py
+++ redis_test_1.py
@@ -1,21 +1,8 @@
-from datetime import datetime
 from time import sleep
-import orjson as json
-
 
 def count_words_at_url(url: str, tokens: set):
-    bro_datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # Using underscores and dashes for file name
-    file_name = f"{bro_datetime_str}.txt"
-
-    with open(file_name, 'w') as file:
-        file.write(str(json.dumps(tokens)))
-
+    print(f"BEFORE 15s - {len(tokens)}")
     sleep(15)
-
-    bro_datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # Using underscores and dashes for file name
-    file_name = f"{bro_datetime_str}.txt"
-
-    with open(file_name, 'w') as file:
-        file.write(str(json.dumps(tokens)))
+    print(f"AFTER 15s - {len(tokens)}")
 
     return url
