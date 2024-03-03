134d613f08b476ce8348b93023b690063b0e705b
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-03-03 13:16:52 +0100

    Reorganization

--- redis_test_1.py
+++ redis_test_1.py
@@ -1,6 +1,7 @@
 from time import sleep
 
-def count_words_at_url(url: str, tokens: set):
+
+def count_words_at_url(url: str, tokens: set, tokens2: set):
     print(f"BEFORE 15s - {len(tokens)}")
     sleep(15)
     print(f"AFTER 15s - {len(tokens)}")
