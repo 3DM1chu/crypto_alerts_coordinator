433676e2c00768b4ffadbe64e4e315ac07f25235
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-03-03 13:15:14 +0100

    Reorganization

--- redis_test_1.py
+++ redis_test_1.py
@@ -3,7 +3,7 @@ from time import sleep
 import orjson as json
 
 
-def count_words_at_url(url, tokens):
+def count_words_at_url(url: str, tokens: set):
     bro_datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # Using underscores and dashes for file name
     file_name = f"{bro_datetime_str}.txt"
 
