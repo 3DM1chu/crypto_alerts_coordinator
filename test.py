95ac766193257ccdd4bc00bc036f7d05d13acac9
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-03-03 12:20:38 +0100

    Update notifications

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from time import sleep
 
 import requests
@@ -5,6 +6,34 @@ import requests
 
 def count_words_at_url(url):
     resp = requests.get(url)
+
+    # Convert datetime object to string
+    bro_datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # Using underscores and dashes for file name
+
+    # Define the file name
+    file_name = f"{bro_datetime_str}.txt"
+
+    # Define the content of the file
+    file_content = "This is a sample file content."
+
+    # Write content to the file
+    with open(file_name, 'w') as file:
+        file.write(file_content)
+
     sleep(15)
     print(len(resp.text.split()))
+
+
+    # Convert datetime object to string
+    bro_datetime_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # Using underscores and dashes for file name
+
+    # Define the file name
+    file_name = f"{bro_datetime_str}.txt"
+
+    # Define the content of the file
+    file_content = "This is a sample file content."
+
+    # Write content to the file
+    with open(file_name, 'w') as file:
+        file.write(file_content)
     return len(resp.text.split())
